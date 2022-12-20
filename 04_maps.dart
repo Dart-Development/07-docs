23058bf713ce2d51390bf17743609d8f77e1ec7b
bgarcia95 <bgarcia.95@outlook.com>
2022-12-20 11:52:21 -0600

    understand Map properties and methods

---
+++ 04_maps.dart
@@ -0,0 +1,28 @@
+main() {
+  final person = {
+    'name': 'Brayan',
+    'lastname': 'Garcia',
+    'age': 27,
+  };
+
+  final address = {
+    'city': 'Santa Ana',
+    'country': 'El Salvador',
+  };
+
+  print('Person: $person');
+  print('Length: ${person.length}');
+  print('Keys: ${person.keys}');
+  print('Values: ${person.values}');
+
+  // all key - value pairs from address will be part of person
+  person.addAll(address);
+  print('AddAll: $person');
+
+  person.remove('country');
+  print('remove: $person');
+
+  person.removeWhere((key, value) => key != 'name');
+
+  print('removeWhere: $person');
+}
