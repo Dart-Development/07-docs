6ba1ac1d597e60a235d0454ba0779cb1d6cc03a5
bgarcia95 <bgarcia.95@outlook.com>
2022-12-20 11:02:53 -0600

    understand more methods on Strings

--- 02_string.dart
+++ 02_string.dart
@@ -10,7 +10,21 @@ main() {
   print('Contains B: ${fullname.contains('B', 0)}');
   print('EndsWith a: ${fullname.endsWith('a')}');
 
+  // Adds the pattern we specify to the left or right for each unit missing to match the condition (e.g: 20 characters). In this case the full name including the blank space is of length 13 therefore there are 7 missing 'spaces' so padding will add for each one of these units the pattern we specify on this function.
   print('PadLeft: ${fullname.padLeft(20, '...')}');
-  // Add the pattern to the left or right for each unit missing to match the condition (in this case 20 characters). In this case the full name including the blank space is of length 13 therefore there are 7 missing 'spaces' so padding will add for each one of these units the pattern we specify on this function.
   print('PadRight: ${fullname.padRight(20, '...')}');
+
+  print('Operator []: ${fullname[10]}');
+  // Multiplicate the string count
+  print('Operator *: ${'*' * 10}');
+
+  print('ReplaceAll: ${fullname.replaceAll(RegExp(r'a'), 'e')}');
+  print('SubString: ${fullname.substring(0, 5)}...');
+  print('indexOf B: ${fullname.indexOf('B')}');
+
+  print('Split: ${fullname.split(' ')}');
+  print('Split: ----${fullname.split(' ')[1]}----');
+
+  print(
+      'Capitalize last letter: ${fullname[fullname.length - 1].toUpperCase()}');
 }
