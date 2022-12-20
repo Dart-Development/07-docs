106b375c445d731ac9738a9760118d6c39fd8927
bgarcia95 <bgarcia.95@outlook.com>
2022-12-20 10:27:45 -0600

    understand more double properties

--- 01_double.dart
+++ 01_double.dart
@@ -1,5 +1,21 @@
 main() {
-  double number = 3.1416;
+  double number = 2.1416;
+  double infinite = double.infinity;
 
   print('Firma: ${number.sign} :: $number');
+
+  print('isFinite: ${number.isFinite} :: $number');
+  print('isFinite: ${infinite.isFinite} :: $infinite');
+
+  print('abs: ${number.abs()} :: $number');
+  print('ceil: ${number.ceil()} :: $number');
+
+  // print('ceil: ${infinite.ceil()} :: $infinite');
+
+  print('ceilToDouble: ${number.ceilToDouble()} :: $number');
+
+  print('round: ${number.round()} :: $number');
+  print('round: ${number.roundToDouble()} :: $number');
+
+  print('clamp: ${number.clamp(1, 3)} :: $number');
 }
