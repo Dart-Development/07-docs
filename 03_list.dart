0bf6d6c1c536bdcd9b43e7ed815a0ec0d41285c1
bgarcia95 <bgarcia.95@outlook.com>
2022-12-20 11:32:47 -0600

    understand more methods and properties from lists

--- 03_list.dart
+++ 03_list.dart
@@ -19,5 +19,38 @@ main() {
   print('listMap: ${listMap[4]}');
 
   Map namesMap = names.asMap();
+  print('namesMap: ${namesMap}');
   print('namesMap: ${namesMap[1]}');
+
+  print(
+      'indexOf: ${names.indexOf('Peter')}'); // Everything on Dart is case sensitive. -1 means not found.
+
+  int greaterThan3 = list1.indexWhere((num) => num > 3);
+
+  print(
+      'indexWhere greater 3: index -> $greaterThan3 - value -> ${list1[greaterThan3]}');
+
+  print('Remove: ${names.remove('Peter')}');
+  print('Remove: ${names}');
+
+  list1.shuffle();
+  print('Shuffle: $list1');
+
+  list3.sort();
+  print('Sort: $list3');
+  // reversed returns an iterable '()' that's we transform it back into a list
+  print('Reverse: ${list3.reversed.toList()}');
+
+  // Objects are passed by reference in Dart
+
+  names.forEach((name) {
+    name = name.toUpperCase();
+    print(name);
+  });
+
+  print('Names List: $names');
+
+  // this is a total new list, no relation with the original 'names' list.
+  final newNamesList = names.map((name) => name.toUpperCase()).toList();
+  print('New Names List: $newNamesList');
 }
